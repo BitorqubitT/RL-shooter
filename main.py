5798d4289c934bf52ddd6e6e0560bd183f34a5a8
BitorqubitT <thierryars2@hotmail.com>
2026-01-10 17:48:15 +0100

    get pygame working

--- main.py
+++ main.py
@@ -2,6 +2,7 @@ import pygame
 from PIL import Image
 import os
 from game_env import Environment
+import numpy as np
 
 clock = pygame.time.Clock()
 FPS = 60
@@ -9,49 +10,36 @@ running = True
 RENDER = True
 env = Environment(["badboy", "goodboy", "goodestboy", "badboy1", "goodestboy3", "feelsboy"], 1280, 1024, RENDER)
 i = 0
+
+pygame.init()
+#display = pygame.display.set_mode((1280, 1024))
+display = pygame.display.set_mode((1280, 1024))
+#obs = np.zeros((1024, 1280, 3), dtype=np.uint8)
+#surf = pygame.surfarray.make_surface(obs)
+
 while running:
-    #for event in pygame.event.get():
-        #if event.type == pygame.QUIT:
-         #   running = False
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            running = False
 
     #keys = pygame.key.get_pressed()
     keys = 4
     obs, reward, cap1, cap2, cap3 = env.step(keys)
     print(reward)
-    # Save frame as image
-    #frame_image = Image.fromarray(obs['agent'], 'RGB')
-    frame_image = Image.fromarray(obs)
-    frame_image.save(f"frames_per_run/frame_{i}.png")
-    i += 1
+
+
+
+    surf = pygame.surfarray.make_surface(obs.swapaxes(0, 1))
+
+    display.blit(surf, (0, 0))
+    pygame.display.flip()
+
     clock.tick(FPS)
+
 pygame.quit()
 
-"""
-To think:
-The game doesnt really need rounds or whatever. But giving the agent a complete new env etc might be important
+    # Save frame as image
+    #frame_image = Image.fromarray(obs['agent'], 'RGB')
+    #frame_image = Image.fromarray(obs)
+    #frame_image.save(f"frames_per_run/frame_{i}.png")
 
-FIXME:
-
-TODO:
-#1
-- Create nicer world
-- Give every agent a color
-- Create first RL version, shoot standint still agents
-- Make sure I can replay a game.
-    - Not by using images (too static)
-    - Get starting position and then all actions
-
-#2
-- Make agents move randomly and shoot?(path tracing)
-
-
-#IDEAS:
-
-- Add matrix world or different vectors
-- Make sure everything works for multiple agents (battle at the same time, train)
-- add replay system.
-Do I need to start making optimisations?
-Matrix operations only?
-Team based?
-
-"""
